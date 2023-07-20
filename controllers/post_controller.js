2d1dfe526e878e21a3401e636d8a712e8ef019d9
Piyush-Jadhav2804 <71430828+Piyush-Jadhav2804@users.noreply.github.com>
2023-07-20 21:20:12 +0530

    Added mongoStore for persistent storage

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,53 +1,50 @@
-const Post = require('../models/post');
-const Comment = require('../models/comment');
+const Post = require("../models/post");
+const Comment = require("../models/comment");
 
-module.exports.create = function(req, res) {
-
-    Post.create({
-        content: req.body.content,
-        user: req.user._id
-    });
-
-    if(req.xhr) {
-        return res.status(200).json({
-            data: {
-                post: post
-            },
-            message: 'Post created'
+module.exports.create = async function (req, res) {
+  
+    try {
+        let post = await Post.create({
+            content: req.body.content,
+            user: req.user._id,
         });
-    }
 
-    req.flash('success', 'Post published');
-
-    return res.redirect('back');
-}
-
-
-module.exports.destroy = function(req, res) {
-
-    Post.findById(req.params.id).then((post) => {
-
-        // .id means converting the object id into string
-        if(post.user == req.user.id) {
-
-            console.log(req.params.id);
-            console.log(req.user.id);
-
-            Post.findByIdAndDelete(req.params.id).then(()=>{
-                
-                Comment.deleteMany({post: req.params.id}).then(() => {
-                    req.flash('success', 'Post deleted');
-                    return res.redirect('back');
-                });
-                
-                console.log('Post deleted');
+        if (req.xhr) {
+            return res.status(200).json({
+                data: {
+                post: post,
+                },
+                message: "Post created",
             });
-
         }
 
-        else {
-            return res.redirect('back');
-        }
-            
-    });
-}
+        req.flash("success", "Post published"); 
+        return res.redirect("back");
+    } 
+    
+    catch (err) {
+        req.flash("error", err);
+        return res.redirect("back");
+    }
+};
+
+module.exports.destroy = function (req, res) {
+  Post.findById(req.params.id).then((post) => {
+    // .id means converting the object id into string
+    if (post.user == req.user.id) {
+      console.log(req.params.id);
+      console.log(req.user.id);
+
+      Post.findByIdAndDelete(req.params.id).then(() => {
+        Comment.deleteMany({ post: req.params.id }).then(() => {
+          req.flash("success", "Post deleted");
+          return res.redirect("back");
+        });
+
+        console.log("Post deleted");
+      });
+    } else {
+      return res.redirect("back");
+    }
+  });
+};
--- index.js
+++ index.js
@@ -1,55 +1,62 @@
 // This project is made by PIYUSH JADHAV
 
-const express = require('express');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = 8000;
-const expressLayouts = require('express-ejs-layouts');
-const db = require('./config/mongoose');
-//used for session cookie
-const session = require('express-session');
-const passport = require('passport');
-const passportLocal = require('./config/passport-local-strategy');
-const MongoStore = require('connect-mongo');
-const sassMiddleware = require('node-sass-middleware');
-const flash = require('connect-flash');
-const customMiddleware = require('./config/middleware');
- 
-//MIDDLEWARES
+const expressLayouts = require("express-ejs-layouts");
+const db = require("./config/mongoose");
+// used for session cookie
+const session = require("express-session");
+const passport = require("passport");
+const passportLocal = require("./config/passport-local-strategy");
+const MongoStore = require("connect-mongo");
+const sassMiddleware = require("node-sass-middleware");
+const flash = require("connect-flash");
+const customMiddleware = require("./config/middleware");
 
-app.use(sassMiddleware({
-    src: './assets/scss',
-    dest: './assets/css',
+// MIDDLEWARES
+
+app.use(
+  sassMiddleware({
+    src: "./assets/scss",
+    dest: "./assets/css",
     debug: true,
-    outputStyle: 'extended',
-    prefix: '/css'
-}));
+    outputStyle: "extended",
+    prefix: "/css",
+  })
+);
 
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: false }));
 
 app.use(cookieParser());
 
-app.use(express.static('./assets'))
+app.use(express.static("./assets"));
 
 app.use(expressLayouts);
-//extract styles & scripts from sub-pages into the layout
-app.set('layout extractStyles', true);
-app.set('layout extractScripts', true);
+// extract styles & scripts from sub-pages into the layout
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
-app.set('view engine', 'ejs');
-app.set('views', './views');
-
-//mongo store is used to store the session cookie in the db
-app.use(session({
-    name: 'socialmediaapp',
-    secret: 'something',
+// mongo store is used to store the session cookie in the db
+app.use(
+  session({
+    name: "socialmediaapp",
+    secret: "something",
     saveUninitialized: false,
     resave: false,
     cookie: {
-        maxAge: (1000 * 60 * 100)
+      maxAge: 1000 * 60 * 100,
     },
-}));
+    store: MongoStore.create({
+        mongoUrl: 'mongodb://127.0.0.1/social_media_application', // Replace this with your MongoDB connection string
+        autoRemove: 'disabled',
+    }),
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -59,14 +66,13 @@ app.use(passport.setAuthenticatedUser);
 app.use(flash());
 app.use(customMiddleware.setFlash);
 
-//use express router
-app.use('/', require('./routes'));
+// use express router
+app.use("/", require("./routes"));
 
-app.listen(port, function(err){
+app.listen(port, function (err) {
+  if (err) {
+    console.log(`Error: ${err}`);
+  }
 
-    if(err) {
-        console.log(`Error : ${err}`);
-    }
-
-    console.log(`Server is up and running on port: ${port}`); 
+  console.log(`Server is up and running on port: ${port}`);
 });
